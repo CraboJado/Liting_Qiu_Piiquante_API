50a6815cee2824d49a51ff85a190519692149dda
lq <liting_qiu@hotmail.com>
2022-05-30 23:40:24 +0200

    multer middleware

---
+++ middlewares/multer.js
@@ -0,0 +1,31 @@
+const multer = require('multer');
+
+const MIME_TYPES = {
+    'image/jpg' :'jpg',
+    'image/jpeg' :'jpg',
+    'image/png' :'jpg'
+}
+
+const storage = multer.diskStorage({
+    destination : (req, file, callback) => {
+        callback(null, 'public/images');
+    },
+    filename : (req, file, callback) => {
+        const regEx = /[\.\s-]/g;
+        const name = file.originalname.replace(regEx,'_');
+        const extension = MIME_TYPES[file.mimetype];
+        callback(null, name +'_'+ Date.now() +'.'+ extension);
+    }
+})
+
+const fileFilter = (req, file, callback) => {
+    if((file.mimetype).includes('jpeg') || (file.mimetype).includes('png') || (file.mimetype).includes('jpn')){
+        callback(null, true);
+    }else {
+        callback(null, false);
+    }
+}
+
+const upload = multer({ storage , fileFilter})
+
+module.exports = upload.single('image');
