84a8e233ad516c53718b91e2260dbc2f0f5c4e52
lq <liting_qiu@hotmail.com>
2022-05-30 23:52:37 +0200

    add sauceSchema

---
+++ models/sauce.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+const sauceSchema = new Schema({
+    userId: { type:String, required:true,unique:true },
+    name: { type:String, required:true },
+    manufacturer: { type:String, required:true },
+    description: { type:String, required:true },
+    mainPepper: { type:String, required:true },
+    imageUrl: { type:String, required:true },
+    heat: Number,
+    likes: Number,
+    dislikes: Number,
+    usersLiked : Array,
+    usersDisliked : Array,
+})
+
+module.exports = mongoose.model('Sauce',sauceSchema);
