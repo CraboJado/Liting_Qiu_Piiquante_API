c11034eea6eb1986eba309503703c04af61cc68d
lq <liting_qiu@hotmail.com>
2022-06-07 15:20:41 +0200

    add comments

--- models/user.js
+++ models/user.js
@@ -7,6 +7,7 @@ const userSchema = new Schema({
     password: { type: String, required:true}
 })
 
+// validator for declared unique fileds
 userSchema.plugin(uniqueValidator);
 
 module.exports = mongoose.model('User',userSchema);
