dfd94ddbb1ed2ce483c669860360991275743dff
lq <liting_qiu@hotmail.com>
2022-06-07 15:19:56 +0200

    add questions

--- server.js
+++ server.js
@@ -11,9 +11,9 @@ dotenv.config();
 mongoose.connect(`mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@${process.env.DB_CLUSTER}.dkktm.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`)
   .then( ( ) => console.log('Successfully connect to mongoDB database '))
   .catch( ( error ) => console.log( error + 'Unsuccessfully connect to mongoDB database')) 
-  // dans le doc., handleError(error) , qu'est ce qu'on peut faire dans la fonction handleError
+  // QQ : dans le doc., handleError(error) , qu'est ce qu'on peut faire dans la fonction handleError
 
-// pourquoi normalizePort ? si je utilise diretement process.env.PORT || '3000', il aura quoi comme problem ?
+// QQ : pourquoi normalizePort ? si je utilise diretement process.env.PORT || '3000', il aura quoi comme problem ?
 const normalizePort = val => {
     const port = parseInt(val, 10);
     if (isNaN(port)) {
@@ -29,7 +29,7 @@ const normalizePort = val => {
 
   const errorHandler = error => {
     if (error.syscall !== 'listen') {
-      throw error;
+      throw error; // QQ : error pass par où ? la ligne 59 ?
     }
     //console.log(address) output : null
     const address = server.address();
@@ -50,7 +50,7 @@ const normalizePort = val => {
   };
 
 // app.set('port', process.env.PORT || 3000); 
-app.set('port', port); // pourquoi , car sans app.set('port'), ca marche aussi ???
+app.set('port', port); // QQ : pourquoi , car sans app.set('port'), ca marche aussi ???
 
 // create a server node : http server and pass express app to handle incoming http request
 const server = http.createServer(app);
@@ -60,10 +60,11 @@ server.on('error',errorHandler);
 server.on('listening', () => {
     //console.log(address) : { address: '::', family: 'IPv6', port: 3000 }
     const address = server.address(); 
-    const bind = typeof address === 'string' ? 'pipe ' + address : 'port ' + port; // example de pipe ??
+    const bind = typeof address === 'string' ? 'pipe ' + address : 'port ' + port; // QQ : example de pipe ??
     console.log('Listening on ' + bind);
   });
 
-server.listen(port);  // la différence par rapport à la ligne 66 ?
-// server.listen(process.env.PORT || 3000);
+// server.listen(port);  // QQ : la différence par rapport à la ligne 68 ?
+server.listen(process.env.PORT || 3000);
+
 
