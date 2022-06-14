51f1fd5ceb008b8d3edf1af0f07bdf814816a060
lq <liting_qiu@hotmail.com>
2022-06-15 00:09:48 +0200

    add helmet and errorHandler

--- app.js
+++ app.js
@@ -1,14 +1,21 @@
 const express = require('express');
 const path = require('path');
+const helmet = require('helmet')
 const userRoutes = require('./routes/user');
 const sauceRoutes = require('./routes/sauce');
+// const { errorHandler } = require('./middlewares/errorHandler');
+const errorHandler = require('./middlewares/error');
+
 
 const app = express();
+
+// Security HTTP Headers
+app.use(helmet());
 
 // parse req.body for JSON format
 app.use(express.json());
 // parse req.body for urlencoded format
-app.use(express.urlencoded({ extended: true }));
+// app.use(express.urlencoded({ extended: true }));
 
 // serve static files
 app.use(express.static(path.join(__dirname, 'public')));
@@ -24,4 +31,6 @@ app.use((req, res, next) => {
 app.use('/api/auth',userRoutes);
 app.use('/api/sauces',sauceRoutes);
 
+// handle error middleware
+app.use(errorHandler);
 module.exports = app;
