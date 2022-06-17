17ce876ca4fe8c8bc9b9a8ae1a0c66cce5705a93
lq <liting_qiu@hotmail.com>
2022-06-17 22:13:25 +0200

    MODIFY

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -4,26 +4,21 @@ const ErrorResponse = require('../utils/errorResponse');
 
 const auth = (req, res, next) => {
       console.log('**************** in auth step****************');
-      console.log('-----> request contente-type ',req.get('Content-Type'));
-      console.log('-----> req.headers.authorization ',req.headers.authorization);
-      console.log('-----> req.body in auth ',req.body);
-      console.log('-----> req.body.sauce in auth ',req.body.sauce);
-
       try {
-        let token;
+          let token;
 
-        if(req.headers.authorization && req.headers.authorization.startsWith('Bearer')){
-          token = req.headers.authorization.split(' ')[1];
-        }
+          if(req.headers.authorization && req.headers.authorization.startsWith('Bearer')){
+            token = req.headers.authorization.split(' ')[1];
+          }
+    
+          if(!token) {
+            return next(new ErrorResponse("unauthorized request, missing valid credentials",401))
+          }
+
+          const decodedToken = jwt.verify(token, process.env.TOKEN_SECRET);
+          req.auth = { userId : decodedToken.data };
+          next();
   
-        if(!token) {
-          return next(new ErrorResponse("unauthorized request, missing valid credentials",401))
-        }
-
-        const decodedToken = jwt.verify(token, process.env.TOKEN_SECRET);
-        req.auth = { userId : decodedToken.data };
-        next();
-        
       } catch (error) {
           next(error);
       }
