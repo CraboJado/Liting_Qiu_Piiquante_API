2d8beb686c86925880229269dfbe2e5ea7492946
lq <liting_qiu@hotmail.com>
2022-06-20 22:19:08 +0200

    modify

--- config/db.js
+++ config/db.js
@@ -1,5 +1,4 @@
 const mongoose = require('mongoose');
-// const { DB_USERNAME, DB_PASSWORD, DB_CLUSTER, DB_NAME } = require('./env');
 
 const connectMongoDB = () => {
   mongoose.connect(`mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@${process.env.DB_CLUSTER}.dkktm.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`)
