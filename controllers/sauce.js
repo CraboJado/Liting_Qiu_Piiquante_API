d08f9c5957dcafcaa589ee3e1af30648c761fe02
lq <liting_qiu@hotmail.com>
2022-06-21 23:09:22 +0200

    modify likeHandler

--- controllers/sauce.js
+++ controllers/sauce.js
@@ -14,47 +14,12 @@ const unlinkFile = (file,next) => {
     })
 }
 
-
-const likeHandler = (body, sauce) => {
-    const isLiked = sauce.usersLiked.find( element => element === body.userId);
-    const isDisliked = sauce.usersDisliked.find( element => element === body.userId);
-
-    if(body.like === 1){
-        if (isLiked) return next( new ErrorResponse('user already liked the sauce',409) );
-        if (isDisliked) return next( new ErrorResponse('user already disliked the sauce',409) );
-        
-        sauce.likes++;
-        sauce.usersLiked.push(body.userId);
-    }
-
-    if(body.like === -1){
-        if (isDisliked) return next( new ErrorResponse('user already disliked the sauce',409) );
-        if (isLiked) return next( new ErrorResponse('user already liked the sauce',409) );
-
-        sauce.dislikes++;
-        sauce.usersDisliked.push(body.userId);
-    }
-
-    if(body.like === 0){
-        if(!isDisliked && !isLiked ) return next( new ErrorResponse('bad requst, you should like or dislike a sauce before cancling',400) );
-        
-        if(isLiked && !isDisliked){
-            sauce.likes = (sauce.likes === 0) ? 0 : --sauce.likes;
-            sauce.usersLiked = sauce.usersLiked.filter( element => element !== body.userId);    
-        }
-
-        if(isDisliked && !isLiked){
-            sauce.dislikes = (sauce.dislikes === 0) ? 0 : --sauce.dislikes;
-            sauce.usersDisliked = sauce.usersDisliked.filter( element => element !== body.userId);
-        }
-    }
-
-}
-
 const updateHandler = (req,res,next) => {
     let filePath
     Sauce.findOneAndUpdate({ _id:req.params.id, userId: req.auth.userId },{ ...req.body },{ returnDocument :'before' })
     .then( sauce => {
+        // when the sauce is not created by the user, user is not allowed to modify the sauce 
+        // and need to delete the uploaded file if file is uploaded
         if(!sauce && !req.file) {
             return next( new ErrorResponse('no matched query',404))
         } 
@@ -65,6 +30,7 @@ const updateHandler = (req,res,next) => {
             return next( new ErrorResponse('no matched query',404))
         }
 
+        // when the sauce is created by the user, user is allowed to modify the sauce
         if(req.file){
             const imageFilename = sauce.imageUrl.split('/images/')[1];
             filePath = getFilePath(imageFilename);
@@ -82,6 +48,34 @@ const updateHandler = (req,res,next) => {
      });
 }
 
+const likeHandler = (body, sauce) => {
+    const isLiked = sauce.usersLiked.find( element => element === body.userId);
+    const isDisliked = sauce.usersDisliked.find( element => element === body.userId);
+
+    if(body.like === 1){
+        sauce.likes++;
+        sauce.usersLiked.push(body.userId);
+    }
+
+    if(body.like === -1){
+        sauce.dislikes++;
+        sauce.usersDisliked.push(body.userId);
+    }
+
+    if(body.like === 0){
+        if(isLiked && !isDisliked){
+            sauce.likes = (sauce.likes === 0) ? 0 : --sauce.likes;
+            sauce.usersLiked = sauce.usersLiked.filter( element => element !== body.userId);    
+        }
+
+        if(isDisliked && !isLiked){
+            sauce.dislikes = (sauce.dislikes === 0) ? 0 : --sauce.dislikes;
+            sauce.usersDisliked = sauce.usersDisliked.filter( element => element !== body.userId);
+        }
+    }
+
+}
+
 exports.getAllSauces = (req, res, next) => {
     Sauce.find()
         .then( sauces => res.status(200).json(sauces) )
@@ -89,7 +83,6 @@ exports.getAllSauces = (req, res, next) => {
 }
  
 exports.createSauce = (req, res, next) => {
-    console.log('***** in createSauce controller ******');
     try {
         if(!req.file) return next( new ErrorResponse('please provide a file',400) );
 
@@ -118,7 +111,10 @@ exports.createSauce = (req, res, next) => {
 
         sauce.save()
         .then( () => res.status(201).json({ message:' sauce created successfully ' }) )
-        .catch( error => next(error) );
+        .catch( error => {
+            unlinkFile(filePath,next);
+            next(error)
+        } );
 
     } catch (error) {
         next(error)
@@ -135,10 +131,8 @@ exports.getSauce = (req, res, next) => {
 }
 
 exports.modifySauce = (req, res, next) => {
-    console.log('************ in modifySauce controller **************');
-    console.log('************ req.body **************',req.body);
-    // if update without image file
     try{
+        // if update without image file
         if(!req.file) {
             const { userId } = req.body; 
 
@@ -148,7 +142,8 @@ exports.modifySauce = (req, res, next) => {
 
             return
         }
-    // if update with image file
+
+    // when update with image file, need to parse body to get userId
         let filePath = getFilePath(req.file.filename);
 
         if(!req.body.sauce){
@@ -178,10 +173,9 @@ exports.modifySauce = (req, res, next) => {
 }
 
 exports.deleteSauce = (req, res, next) => {
-    console.log('************ in deleteSauce controller **************');
     Sauce.findOneAndDelete({ _id: req.params.id, userId: req.auth.userId })
     .then( sauce => {
-        if (!sauce) return next( new ErrorResponse('no matched query',404));
+        if (!sauce) return next( new ErrorResponse('no matched query',404) );
 
         const imageFilename = sauce.imageUrl.split('/images/')[1];
         const filePath = getFilePath(imageFilename);
@@ -192,36 +186,58 @@ exports.deleteSauce = (req, res, next) => {
 }
 
 exports.likeSauce = (req, res, next) => {
-    console.log('*********** in likeSauce controller ******')
+    if(req.body.userId !== req.auth.userId) 
+    return next( new ErrorResponse('unauthorized request, invalid credentials',401) )
+
     Sauce.findOne({ _id : req.params.id })
     .then ( sauce => {
-        if(!sauce) return Promise.reject(new ErrorResponse('no matched query111',404));
-        likeHandler(req.body,sauce);
+        if(!sauce) return Promise.reject(new ErrorResponse('no matched query',404));
+
+        const isLiked = sauce.usersLiked.find( element => element === req.body.userId);
+        const isDisliked = sauce.usersDisliked.find( element => element === req.body.userId);
+
+        if(req.body.like === 1){
+            if (isLiked) return Promise.reject( new ErrorResponse('can not like the sauce twice',409) );
+            if (isDisliked) return Promise.reject( new ErrorResponse('can not like the sauce you disliked',409) );
+        }
+
+        if(req.body.like === -1){
+            if (isDisliked) return Promise.reject( new ErrorResponse('can not dislike the sauce twice',409) );
+            if (isLiked) return Promise.reject( new ErrorResponse('can not dislike the sauce you liked ',409) );
+        }
+
+        if(req.body.like === 0){
+            if(!isDisliked && !isLiked ) return Promise.reject( new ErrorResponse('bad requst',400) );
+        }
+
+        likeHandler(req.body, sauce);
+
         const updateObj = {
             likes: sauce.likes,
             dislikes: sauce.dislikes,
             usersLiked: sauce.usersLiked,
             usersDisliked: sauce.usersDisliked,
         }
-        return updateObj
+
+        return updateObj 
     })
     .then( updateObj => {
-        console.log('------> SupdateObj from uper then',updateObj);
         Sauce.updateOne({_id : req.params.id},{...updateObj})
         .then(() => {
             if(req.body.like === 1) {
-                res.status(201).json({ message:'you like sauce' })
+                res.status(201).json({ message:'like sauce successfully' })
             }
+
             if(req.body.like === -1) {
-                res.status(201).json({ message:'you dislike sauce' })
+                res.status(201).json({ message:'dislike sauce successfully' })
             }
+
             if(req.body.like === 0) {
-                res.status(201).json({ message:'your cancle is done' })
+                res.status(201).json({ message:'cancle successfully' })
             } 
         })
         .catch( error => next(error) )
     })
-    .catch( error => next(error) )
-        
+    .catch( error => next(error) )   
 }
 
