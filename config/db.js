a63caf72e026135b9dd4df8de7b7ade813285326
liting <liting_qiu@hotmail.com>
2023-01-09 13:07:12 +0100

    modify config

--- config/db.js
+++ config/db.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const connectMongoDB = () => {
   mongoose.connect(`mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@${process.env.DB_CLUSTER}.dkktm.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`)
   .then( ( ) => console.log('Successfully connect to mongoDB database '))
-  .catch( ( error ) => console.log( error + 'Unsuccessfully connect to mongoDB database')) 
+  .catch( ( error ) => console.log( error + '***Unsuccessfully connect to mongoDB database')) 
 }
 
 module.exports = connectMongoDB;
